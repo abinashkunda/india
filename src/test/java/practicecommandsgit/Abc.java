b57b71f00e4333212ffd23f19e284c4aa53d6aca
abinash kunda <abinashkunda24@gmail.com>
2021-10-29 19:57:02 +0530

    commit6

--- src/test/java/practicecommandsgit/Abc.java
+++ src/test/java/practicecommandsgit/Abc.java
@@ -3,6 +3,7 @@ package practicecommandsgit;
 public class Abc {
 	public static void main(String[] args) {
 		System.out.println("i am doing practice");
+		System.out.println("you are also doing practice");
 	}
 
 }
